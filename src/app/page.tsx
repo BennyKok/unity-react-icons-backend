fb800d8220641bd101422fc4229197de05276ef8
BennyKok <itechbenny@gmail.com>
2023-07-02 15:23:38 +0800

    chore: update typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      A Backend Made for loading React Icons in Unity
+      A Backend made for loading React Icons in Unity
     </main>
   );
 }
